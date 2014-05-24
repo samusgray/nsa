7d1c1e22d4e9d2be0c4baa7256bb5cdea09ebf54
Eric Meyer <eric@oddbird.net>
2014-05-23 21:40:35 -0600

    NSA first draft

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem "sass", "~> 3.3.7"
+gem "compass", "~> 1.0.0.alpha.19"
+gem "accoutrement", "~> 1.0.0.beta.1"
+gem "susy", "~> 2.1.2"
---
+++ config.rb
@@ -0,0 +1,12 @@
+# Compass CSS framework config file
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images/"
+fonts_dir = "fonts/"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
